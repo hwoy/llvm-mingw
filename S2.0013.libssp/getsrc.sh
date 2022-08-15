4b1dc12364f7a39e3d327016c90920870b9059d3
Watthanachai Dueanklang <bosskillerz@gmail.com>
2022-08-15 08:28:46 +0700

    modified:   S2.0013.libssp/getsrc.sh

--- S2.0013.libssp/getsrc.sh
+++ S2.0013.libssp/getsrc.sh
@@ -4,4 +4,4 @@ DIR=$(dirname $0)
 
 wget -P ${DIR} --tries=10 -c -i ${DIR}/wget.txt
 
-mv "gcc-releases-gcc-7.3.0.tar.bz2@path=libssp" libssp-7.3.0.tar.bz2
+mv "${DIR}/gcc-releases-gcc-7.3.0.tar.bz2@path=libssp" ${DIR}/libssp-7.3.0.tar.bz2
